fb62f502b4fd0945bea66d8df14ee14dc99b8cb1
Tijana <tijanaknezevic03@gmail.com>
2026-08-01 22:40:34 +0200

    minor

--- fund/decorators.py
+++ fund/decorators.py
@@ -4,14 +4,14 @@ from flask_jwt_extended import verify_jwt_in_request
 from flask_jwt_extended import jwt_required
 from functools import wraps
 
-def role_check(role):
+def role_check(*roles):
     def decorator(function):
         @jwt_required()
         @wraps(function)
         def wrapper(*args, **kwargs):
             # verify_jwt_in_request ( )
             claims = get_jwt()
-            if (role == claims["role"]):
+            if (claims["role"] in roles):
                 return function(*args, **kwargs)
             else:
                 return jsonify({"msg": "Missing Authorization Header"}), 401
